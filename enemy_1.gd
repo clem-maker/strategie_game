276afe868550009120ca053e465087c36a57ffd3
clem-maker <clemenshillger12@gmail.com>
2026-08-06 17:32:50 +0200

    bug fix
    fixed random y_vel bug and made the code more flexibile

--- enemy_1.gd
+++ enemy_1.gd
@@ -1,20 +1,21 @@
 extends CharacterBody2D
 var y_vel  : float = 0
 var y_time : float = 1
-
+func _ready() -> void:
+	print($MeshInstance2D.mesh.height)
 func y_vel_manager(delta:float) ->float:
-	y_time -= delta
-	if y_time <= 0:
-		y_vel = randfn(-80,80)
-		if y_vel + position.y > 648 \
-		or y_vel + position.y < 0:
-			y_vel *= -1
-		y_time = 0
+	y_vel = randfn(-40,40)
+	if ( position.y + (y_vel * delta) > 648 - ($MeshInstance2D.mesh.height)  # wenn y_velocity auserhalb des windows gehen mochte flippen (*-1)
+	or   position.y - (y_vel * delta) <   0 + ($MeshInstance2D.mesh.height) ):
+		y_vel *= -1
 		
+	y_time = 1
 	return y_vel
 
 func _physics_process(delta: float) -> void:
 	velocity = Vector2(-80,y_vel)
 	move_and_slide()
 	
-	y_vel = y_vel_manager(delta)
+	y_time -= delta
+	if y_time <= 0:
+		y_vel = y_vel_manager(delta)
