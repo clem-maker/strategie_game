3c2aa554abf65fbfa5e449a31de9eb93679c19f2
clem-maker <clemenshillger12@gmail.com>
2026-08-06 17:50:23 +0200

    small bug fixes

--- enemy_1.gd
+++ enemy_1.gd
@@ -2,7 +2,12 @@ extends CharacterBody2D
 var y_vel  : float = 0
 var y_time : float = 1
 func _ready() -> void:
-	print($MeshInstance2D.mesh.height)
+	position = Vector2( get_window().size.x , get_window().size.y /2 )
+	
+	
+	
+	
+	
 func y_vel_manager(delta:float) ->float:
 	y_vel = randfn(-40,40)
 	if ( position.y + (y_vel * delta) > 648 - ($MeshInstance2D.mesh.height)  # wenn y_velocity auserhalb des windows gehen mochte flippen (*-1)
