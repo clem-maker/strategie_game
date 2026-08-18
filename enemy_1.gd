a6c502151f309740bc8a607e2d3fb6d87bcf6593
clem-maker <clemenshillger12@gmail.com>
2026-08-18 21:26:56 +0200

    stuff

--- enemy_1.gd
+++ enemy_1.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 var y_vel  : float = 0
 var y_time : float = 1
 func _ready() -> void:
-	position = Vector2( get_window().size.x , get_window().size.y /2 )
+	position = Vector2( get_window().size.x , get_window().size.y /2. )
 	
 	
 	
 	
 	
 func y_vel_manager(delta:float) ->float:
-	y_vel = randfn(40,-45)
+	y_vel = randfn(10,-10)
 	if ( position.y + (y_vel * delta) > 648 - ($MeshInstance2D.mesh.height)  # wenn y_velocity auserhalb des windows gehen mochte flippen (*-1)
 	or   position.y - (y_vel * delta) <   0 + ($MeshInstance2D.mesh.height) ):
 		y_vel *= -1
--- player.gd
+++ player.gd
@@ -1,15 +1,14 @@
 extends CharacterBody2D
 
 var target : CharacterBody2D
-
+var richtungs_vec2 : Vector2
 func _ready() -> void:
+	
 	position = Vector2( (get_window().size.x / 3 ) , (get_window().size.y / 2 ) )
 	target = $"../enemy_1"
-	
-	
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if is_instance_valid(target):
-		var richtungs_vec2 : Vector2 = target.position
-		
-
-	
+		richtungs_vec2 = target.position - position
+		queue_redraw()
+func _draw() -> void:
+	draw_line( Vector2.ZERO ,richtungs_vec2,Color.WHITE,1)
